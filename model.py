8cfb23ed04c9edd1bdd06385da2b773014dfb2a7
Brett Ashley McCoubrey <brettashley@Bretts-MacBook.local>
2018-11-18 17:13:27 -0800

    move files

--- model.py
+++
@@ -1,7 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-import pickle
-
-def unpickle():
-    path = './model.pkl'
-    model_unpickle = open(path, 'rb')
-    return pickle.load(model_unpickle)
